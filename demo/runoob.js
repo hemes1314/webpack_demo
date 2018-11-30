b2f03d761d61adda5616545ca83aeac0ec078c0f
wubin <wubb@sino-bridge.com>
2018-11-30 18:11:43 +0800

    1

---
+++ demo/runoob.js
@@ -0,0 +1,13 @@
+// npm install npm -g
+// cnpm install webpack@3.0.0 -g   查看插件版本列表：npm view webpack versions 已安装插件版本： npm ls webpack
+// webpack runoob.js bundle.js
+// npm install css-loader style-loader -g
+/*
+# 安装
+npm install webpack-dev-server@2.11.3 -g
+ 
+# 运行
+webpack-dev-server --progress --colors
+*/
+document.write("It works.");
+
